d08484e65aef983e85a4c6f5c84af806112be676
songrim <srimm3399@naver.com>
2024-05-27 11:35:23 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// 클래스 버전
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        Hello, World!
+       </div>
+    );
+  }
 }
+
+
+//함수 버전
+// function App() {
+//   return (
+//     <div className="App">
+//       Hello, World!
+//     </div>
+//   );
+// }
 
 export default App;
